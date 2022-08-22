9289f31d6486cae4c9e9dadca199682d320fe2a1
James Scholz <45739456+jvscholz@users.noreply.github.com>
2022-08-21 21:46:04 -0600

    Add files via upload

---
+++ Prework_JamesScholz/Prework_JamesScholzUITests/Prework_JamesScholzUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Prework_JamesScholzUITestsLaunchTests.swift
+//  Prework_JamesScholzUITests
+//
+//  Created by jay on 8/17/22.
+//
+
+import XCTest
+
+class Prework_JamesScholzUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
